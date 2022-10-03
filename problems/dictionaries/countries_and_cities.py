2b8fb593eb5877accd5d59c196fdf53c779dda47
Aleksandr Nokhrin <a.v.nokhrin@yandex.ru>
2022-10-03 08:13:50 +0500

    added one more test using mark.parametrize

--- problems/dictionaries/countries_and_cities.py
+++ problems/dictionaries/countries_and_cities.py
@@ -4,18 +4,29 @@
 Для каждого города укажите, в какой стране он находится.
 """
 
-# collect data for dictionary
-elements_count = int(input())
-countries_cities_dict = {}
-for i in range(elements_count):
-    input_list = input().split(sep=' ')
-    countries_cities_dict[input_list[0]] = input_list[1:]
+def get_country_for_city(input_count: str, country_cities: list, requests_count: str, request_cities: list) -> list:
+    """
+    Return country for requested city.
 
-# requests to dictionary
-requests_count = int(input())
-for i in range(requests_count):
-    city_to_search = input()
-    for country, cities in countries_cities_dict.items():
-        if city_to_search in cities:
-            print(country)
+    :param input_count: number of input of country and it's cities
+    :param country_cities: list of strings <country> <city 1> <city 2> ... <city N>
+    :param requests_count: number of following requests
+    :param request_cities: list of str requests as <city>
+    :return: list of country names, one for each city request
+    """
+    # collect data for dictionary
+    elements_count = int(input_count)
+    countries_cities_dict = {}
+    for i in range(elements_count):
+        input_list = country_cities[i].split(sep=' ')
+        countries_cities_dict[input_list[0]] = input_list[1:]
 
+    # requests to dictionary
+    requests_count = int(requests_count)
+    countries_list = []
+    for i in range(requests_count):
+        city_to_search = request_cities[i]
+        for country, cities in countries_cities_dict.items():
+            if city_to_search in cities:
+                countries_list.append(country)
+    return countries_list
